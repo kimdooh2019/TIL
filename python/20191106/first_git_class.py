31b5f5b49cd1cf8d1b1cef51e46fb7082929a4b1
kimdooh2019 <dopa213@naver.com>
2019-11-06 15:47:10 +0900

    looks like python class

--- python/20191106/first_git_class.py
+++ python/20191106/first_git_class.py
@@ -1,15 +1,19 @@
 
-for i in range(101):
+#for i in range(101):
     
-    if   i % 15 == 0:
-        print(f'fizzbuss{i}')
+#    if   i % 15 == 0:
+#        print(f'fizzbuss{i}')
+#
+#    elif i % 3 == 0 :
+#        print('fizz')
+#    elif i % 5 == 0:
+#        print('buzz')
+#    else:
+#        print(i)
 
-    elif i % 3 == 0 :
-        print('fizz')
-    elif i % 5 == 0:
-        print('buzz')
-    else:
-        print(i)
 
-
-
+#for i in range(101):
+#    if i % 3 == 0 or i % 5 == 0:
+#        print("fizz" *(i % 3 == 0) + "buzz"*(i % 5 == 0))
+    
+print([ 'fizzbuzz' if i%15 == 0 else 'fizz' if i%3==0 else 'buzz' if i%5==0 else i for i in range(1, 101) ])
