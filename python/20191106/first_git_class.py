0ef999e4e29018cf84ecb6a803a964b65ef42419
kimdooh2019 <dopa213@naver.com>
2019-11-06 15:13:26 +0900

    feat: test

---
+++ python/20191106/first_git_class.py
@@ -0,0 +1,15 @@
+
+for i in range(101):
+    
+    if   i % 15 == 0:
+        print(f'fizzbuss{i}')
+
+    elif i % 3 == 0 :
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
+
+
+
