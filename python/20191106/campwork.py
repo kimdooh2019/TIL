e5c3fab35401e7b20cce74eb772c3aee8a35f956
kimdooh2019 <dopa213@naver.com>
2019-11-06 17:56:54 +0900

    commit4

--- python/20191106/campwork.py
+++ python/20191106/campwork.py
@@ -1,12 +1,15 @@
 
-for i in range(1, 101):
-    if i % 15 == 0:
-        print(f'fizzbizz{i}')
-    elif i % 3 == 0:
-        print(f'fizz{i}')
-    elif i % 5 == 0:
-        print(f'bizz{i}')
-    #else:
-    #    print(f'{i}')
+#for i in range(1, 101):
+#    if i % 15 == 0:
+#        print(f'fizzbizz{i}')
+#    elif i % 3 == 0:
+#        print(f'fizz{i}')
+#    elif i % 5 == 0:
+#        print(f'bizz{i}')
+#    else:
+#        print(f'{i}')
 
-    
+ 
+for i in range(1, 101):
+    if i % 3 == 0 or i % 5 == 0:
+        print('fizz'*(i % 3 == 0) + 'bizz' * (i % 5  ==0))
