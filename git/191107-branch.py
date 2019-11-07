619a6fbb36fc8d7ca8830f14d9bd1412bbee09a9
kimdooh2019 <dopa213@naver.com>
2019-11-07 12:26:21 +0900

    fast test

--- git/191107-branch.py
+++ git/191107-branch.py
@@ -7,4 +7,10 @@
 #        print(f'buzz {i}')
 #    else:
 #        print(i)
-print(['fizzbuzz' if i % 15 == 0 else 'fizz' if i % 3 == 0 else 'buzz' if i % 5 == 0 else i for i in range(1, 101) ] )
+#print(['fizzbuzz' if i % 15 == 0 else 'fizz' if i % 3 == 0 else 'buzz' if i % 5 == 0 else i for i in range(1, 101) ] )
+
+print('aaaaaaaaaaaaaaaaaa')
+
+for i in range(1, 101):
+    if i % 3 ==0 or i % 5 ==0:
+        print('fizz'*(i%3==0) + 'buzz'*(i%5==0))
