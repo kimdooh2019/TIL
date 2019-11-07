a8c7222c215a561240b17d31556e0fb1a0797207
kimdooh2019 <dopa213@naver.com>
2019-11-07 11:39:31 +0900

    feat: fizzbuzz range(1,101) 3 or 5 or 15 case divide

--- git/191107-branch.py
+++ git/191107-branch.py
@@ -1,1 +1,9 @@
-print('fizzbizz start from herr')
+for i in range(1, 101):
+    if i % 15 == 0:
+        print(f'fizzbuzz {i}')
+    elif i % 3 == 0:
+        print(f'fizz {i}')
+    elif i % 5 == 0:
+        print(f'buzz {i}')
+    else:
+        print(i)
